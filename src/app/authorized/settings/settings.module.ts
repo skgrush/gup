2f504bf398ccd999e75ab861ee3197ed0d741736
Samuel K. Grush <Samuel@Grush.org>
2020-05-25 18:49:00 -0500

    Fixed imports/exports with SettingsModule.

--- src/app/authorized/settings/settings.module.ts
+++ src/app/authorized/settings/settings.module.ts
@@ -1,13 +1,25 @@
+// External Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthedSettingsComponent } from './components/authed-settings/authed-settings.component';
-import { QrComponent } from './components/qr/qr.component';
-import { TotpQrPopupComponent } from './components/totp-qr-popup/totp-qr-popup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// My Modules
 import { GupCommonModule } from 'src/app/gup-common/gup-common.module';
 import { TabbedComponentModule } from 'src/app/tabbed-component/tabbed-component.module';
 
+// This Module's Declarations
+import { AuthedSettingsComponent } from './components/authed-settings/authed-settings.component';
+import { QrComponent } from './components/qr/qr.component';
+import { TotpQrPopupComponent } from './components/totp-qr-popup/totp-qr-popup.component';
+
 @NgModule({
   declarations: [AuthedSettingsComponent, QrComponent, TotpQrPopupComponent],
-  imports: [CommonModule, GupCommonModule, TabbedComponentModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    GupCommonModule,
+    TabbedComponentModule,
+  ],
+  exports: [AuthedSettingsComponent],
 })
 export class SettingsModule {}
