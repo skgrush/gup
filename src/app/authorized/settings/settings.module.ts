fbd3a53ef89ef135631d48620a7c339492a9e38a
Samuel K. Grush <Samuel@Grush.org>
2020-05-27 18:05:42 -0500

    Moved auth-settings.component to settings.component

--- src/app/authorized/settings/settings.module.ts
+++ src/app/authorized/settings/settings.module.ts
@@ -8,7 +8,7 @@ import { GupCommonModule } from 'src/app/gup-common/gup-common.module';
 import { TabbedComponentModule } from 'src/app/tabbed-component/tabbed-component.module';
 
 // This Module's Declarations
-import { AuthedSettingsComponent } from './components/authed-settings/authed-settings.component';
+import { AuthedSettingsComponent } from './settings.component';
 import { QrComponent } from './components/qr/qr.component';
 import { TotpQrPopupComponent } from './components/totp-qr-popup/totp-qr-popup.component';
 
